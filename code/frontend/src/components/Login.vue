5120019088053ea91755503fc719dce5940a0bdd
Matt Walker <matt.walker98@outlook.com>
2020-11-25 15:15:23 -0500

    Template for modifying sign-in components

--- code/frontend/src/components/Login.vue
+++ code/frontend/src/components/Login.vue
@@ -1,14 +1,38 @@
 <template>
   <v-container fluid class="pa-6">
     <v-row align="center" justify="center">
-      <amplify-authenticator/>
+      <amplify-authenticator>
+        <amplify-sign-in
+          header-text="My Custom Sign In Text"
+          slot="sign-in"
+          :formFields="formFields"
+        ></amplify-sign-in>
+      </amplify-authenticator>
     </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      formFields: [ //THIS BULLSHIT SHOULD WORK BUT IT DOESNT >:(
+        {
+          type: "email",
+          label: "Custom email Label",
+          placeholder: "custom email placeholder",
+          required: true,
+        },
+        {
+          type: "password",
+          label: "Custom Password Label",
+          placeholder: "custom password placeholder",
+          required: true,
+        }
+      ],
+    };
+  },
 };
 </script>
 
