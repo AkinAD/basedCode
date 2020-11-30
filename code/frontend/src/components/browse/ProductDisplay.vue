c718e3797dacfe3e5dc38d588384a6eb026c2036
Matt Walker <matt.walker98@outlook.com>
2020-11-29 19:59:15 -0500

    Implemented Item Modal

--- code/frontend/src/components/browse/ProductDisplay.vue
+++ code/frontend/src/components/browse/ProductDisplay.vue
@@ -1,31 +1,19 @@
 <template>
   <v-container fluid>
-    <v-row>
-      <v-col md="2"> <h3>Products</h3></v-col>
-      <v-col md="15" class="stayBinch">
-        <v-container>
-          <v-toolbar flat color="transparent">
-            <v-toolbar-title class="smallFont"
-              >Search our items ............
-            </v-toolbar-title>
-            <v-btn icon @click="setSearchBoxVisibility(!searchBoxVisible)">
-              <v-icon>mdi-magnify</v-icon>
-            </v-btn>
-            <v-container class="py-0">
-              <v-text-field
-                ref="search"
-                v-model="search"
-                full-width
-                hide-details
-                label="Search"
-                single-line
-                v-show="searchBoxVisible"
-              ></v-text-field>
-            </v-container>
-          </v-toolbar>
-        </v-container>
-      </v-col>
-    </v-row>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h3>Products</h3>
+          <v-text-field
+            ref="search"
+            v-model="search"
+            hide-details
+            label="Search"
+            single-line
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
 
     <v-row>
       <v-col sm="6" md="4" v-for="item in searchFilteredItems" :key="item.id">
---
+++ code/frontend/src/components/cards/VerticalProductModal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary" v-bind="attrs" outlined v-on="on"> info </v-btn>
+      </template>
+
+      <v-card>
+        <v-container fluid mx-auto>
+          <v-card-title class="headline grey lighten-2">
+            {{ product.title }}
+          </v-card-title>
+          <v-card-subtitle> $ {{ product.price }} </v-card-subtitle>
+
+          <v-img :src="product.image" contain height="200px"> </v-img>
+
+          <v-card-text> Item description here </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-text> Location in store </v-card-text>
+
+          <!--in store location component here | pass product as prop-->
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false"> Close </v-btn>
+          </v-card-actions>
+        </v-container>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: Object, //this cause big is shoe
+  },
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
