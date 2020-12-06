5031a05e44f561e21793c7eb8684e06390dfedda
Matt Walker <matt.walker98@outlook.com>
2020-12-06 17:51:56 -0500

    Update App.vue

--- code/frontend/src/App.vue
+++ code/frontend/src/App.vue
@@ -6,22 +6,7 @@
       ><!--where pages are to be loaded-->
       <v-container fluid>
         <router-view />
-      </v-container>
-      <v-container>
-        <!-- <v-btn
-          v-scroll="onScroll"
-          v-show="fab"
-          fab
-          dark
-          fixed
-          bottom
-          right
-          color="primary"
-          @click="toTop"
-        >
-          <v-icon>mdi-chevron-up</v-icon>
-        </v-btn> -->
-      </v-container>
+      </v-container>  
     </v-main>
     <Footer />
   </v-app>
